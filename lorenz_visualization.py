025f49daeaa6570a7aff275433495e496cc88cdf
Henry Cuzco <40706933+uiucmeche1317@users.noreply.github.com>
2025-03-29 00:57:29 -0500

    Fixed plots and added a save figure call

--- lorenz_visualization.py
+++ lorenz_visualization.py
@@ -11,30 +11,42 @@ beta = 8.0 / 3.0
 T = 10.0
 dt = 0.01
 n = int(T / dt)
+y = np.zeros((n,3))
 
 #%%
 # Lorenz function
 def lorenz(t, y):
     
-    dy = [sigma * (y[1] - y[0]), 
+    dy = np.array([sigma * (y[1] - y[0]), 
           y[0] * (rho - y[2]) - y[1],
-          y[0] * y[1] - beta * y[2]]
+          y[0] * y[1] - beta * y[2]])
     
     return dy
+
+# RK4 single step function
+def rk4_singlestep(fun, dt, t0, y0):
+    
+    k1 = fun(t0, y0)
+    k2 = fun(t0 + dt / 2, y0 + dt / 2 * k1)
+    k3 = fun(t0 + dt / 2, y0 + dt / 2 * k2)
+    k4 = fun(t0 + dt, y0 + dt * k3)
+    yout = y0 + dt / 6 * (k1 + 2*k2 + 2*k3 + k4)
+
+    return yout
 #%%
 # Initial guess and time vector
-y0 = [-8, 8, 27]
+y0 = np.array([-8, 8, 27])
+y[0,:] = y0
 t = np.linspace(0, T, n)
 
 #%%
-# Solving Lorenz system with RK45 algorithm under the hood (that's the default solver)
-soln = solve_ivp(lorenz, (0, T), y0, t_eval = t, method = "RK45")
-y = soln.y.T
+for i in range(n - 1):
+    y[i+1,:] = rk4_singlestep(lorenz, dt, t[i], y[i,:])
 # %%
 # Load Fortran data
 fdata = np.loadtxt("./lorenz_data.dat")
 #%%
-fig1 = plt.figure()
+fig1 = plt.figure(figsize=(8,10))
 ax1 = fig1.add_subplot(projection = "3d")
 ax1.plot(fdata[:,0], fdata[:,1], fdata[:,2], "b", label = "Fortran Data")
 ax1.plot(y[:,0], y[:,1], y[:,2], "ro", alpha = 0.30, label = "Python Data")
@@ -44,7 +56,8 @@ ax1.set_zlabel("z")
 ax1.set_title("Lorenz Attractor")
 ax1.legend()
 plt.tight_layout()
-plt.show()
+plt.show(block = False)
+fig1.savefig("lorenz_system.svg")
 
 # %%
 res = fdata - y
@@ -58,7 +71,7 @@ def plot_residual(ax, t, residuals, state_label):
     ax.set_ylabel("Residual")
     ax.set_title(f"Residual of state {state_label}")
 
-fig2, ax2 = plt.subplots(3, 1, figsize=(8, 10))  # Create subplots for x, y, z
+fig2, ax2 = plt.subplots(3, 1, figsize=(8,10)) # Create subplots for x, y, and z
 
 states = ['x', 'y', 'z']
 for i in range(3):
@@ -66,3 +79,4 @@ for i in range(3):
 
 plt.tight_layout()
 plt.show()
+fig2.savefig("residuals_plot.svg")
